41e0a84de33b7d7c7bf3087e66f766f9c9b4c02f
omisan01 <omkar12mitake@gmail.com>
2025-10-17 15:57:44 +0530

    contact form

--- public/js/form.js
+++ public/js/form.js
@@ -1,20 +1,40 @@
-/** Admission Enquiry API Call (axios) */
+// ✅ Admission Enquiry API Call (axios)
 export const admissionEnquiryRequest = async (data) => {
   try {
     const config = {
       method: "post",
       url: "https://sis.simplyadmission.com/",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "x-api-key": "aAKb2GZ4O5O69NZ6SxFe9BrA69z1RhQy5hULNcpSt8QiStzY83",
+        "Content-Type": "application/json",
+      },
       data,
     };
 
-    // Make API Call
     const response = await axios.request(config);
     return response.data;
-
-    // Error Handling
   } catch (error) {
-    error.response.data.status = false;
-    return error.response;
+    console.error("❌ API error:", error);
+    return { status: false, message: error.message };
+  }
+};
+
+export const contactEnquiryRequest = async (data) => {
+  try {
+    const config = {
+      method: "post",
+      url: "https://sis.simplyadmission.com/",
+      headers: {
+        "x-api-key": "aAKb2GZ4O5O69NZ6SxFe9BrA69z1RhQy5hULNcpSt8QiStzY83",
+        "Content-Type": "application/json",
+      },
+      data,
+    };
+
+    const response = await axios.request(config);
+    return response.data;
+  } catch (error) {
+    console.error("❌ API error:", error);
+    return { status: false, message: error.message };
   }
 };
