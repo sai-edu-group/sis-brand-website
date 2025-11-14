ca61ca74c0620cc0856d4ceae58fe24ee8e8d79c
omisan01 <omkar12mitake@gmail.com>
2025-11-14 22:27:15 +0530

    team fixes

--- src/data/about/member/team.ts
+++ src/data/about/member/team.ts
@@ -1,45 +1,44 @@
-export const teamDetails = {
-  sectionHeaderDetails: {
-    eyebrowText: "Key Personnel",
-    title: "Leading with Vision and Commitment",
-  },
-  teamMembers: [
-    {
-      name: "Shri Nilakantha Panigrahi",
-      position: "Director Academics & Senior Principal",
-      quote:
-        "Education is not just about imparting knowledge, it's about igniting curiosity and nurturing the potential within every child.",
-      img: "/images/team/panigrahi.png",
-      bgImg: "/images/team/team-bg.png",
-      jpgImg: "/images/team/panigrahi.jpg",
-      isActive: true,
-    },
-    {
-      name: "Dr. Silpi Sahoo",
-      position: "Chairperson",
-      quote:
-        "Education is not just about imparting knowledge, it's about igniting curiosity and nurturing the potential within every child.",
-      img: "/images/team/silpi.png",
-      bgImg: "/images/team/forest-bg.png",
-      jpgImg: "/images/team/silpi.jpg",
-    },
-    {
-      name: "Shri Prakash Sahoo",
-      position: "Associate Director & Principal Secondary",
-      quote:
-        "Education is not just about imparting knowledge, it's about igniting curiosity and nurturing the potential within every child.",
-      img: "/images/team/prakash-sahoo.png",
-      bgImg: "/images/team/wework-bg.png",
-      jpgImg: "/images/team/prakash.jpg",
-    },
-    {
-      name: "Smt Rashmi Pandey",
-      position: "Associate Director & Principal Primary",
-      quote:
-        "Education is not just about imparting knowledge, it's about igniting curiosity and nurturing the potential within every child.",
-      img: "/images/team/rashmi.png",
-      bgImg: "/images/team/office-bg.png",
-      jpgImg: "/images/team/rashmi.jpg",
-    },
-  ],
+export const sectionHeaderDetails = {
+  eyebrowText: "Key Personnel",
+  title: "Leading with Vision and Commitment",
 };
+
+export const teamMembers = [
+  {
+    name: "Shri Nilakantha Panigrahi",
+    position: "Director Academics & Senior Principal",
+    quote:
+      "Education is not just about imparting knowledge, it's about igniting curiosity and nurturing the potential within every child.",
+    img: "/images/team/panigrahi.png",
+    bgImg: "/images/team/team-bg.png",
+    jpgImg: "/images/team/panigrahi.jpg",
+    isActive: true,
+  },
+  {
+    name: "Dr. Silpi Sahoo",
+    position: "Chairperson",
+    quote:
+      "Education is not just about imparting knowledge, it's about igniting curiosity and nurturing the potential within every child.",
+    img: "/images/team/silpi.png",
+    bgImg: "/images/team/forest-bg.png",
+    jpgImg: "/images/team/silpi.jpg",
+  },
+  {
+    name: "Shri Prakash Sahoo",
+    position: "Associate Director & Principal Secondary",
+    quote:
+      "Education is not just about imparting knowledge, it's about igniting curiosity and nurturing the potential within every child.",
+    img: "/images/team/prakash-sahoo.png",
+    bgImg: "/images/team/wework-bg.png",
+    jpgImg: "/images/team/prakash.jpg",
+  },
+  {
+    name: "Smt Rashmi Pandey",
+    position: "Associate Director & Principal Primary",
+    quote:
+      "Education is not just about imparting knowledge, it's about igniting curiosity and nurturing the potential within every child.",
+    img: "/images/team/rashmi.png",
+    bgImg: "/images/team/office-bg.png",
+    jpgImg: "/images/team/rashmi.jpg",
+  },
+];
