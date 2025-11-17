d99a0f94621433909f9d85a565fe88acf6d89b67
omisan01 <omkar12mitake@gmail.com>
2025-11-17 21:18:45 +0530

    real data

--- src/data/home/awards.ts
+++ src/data/home/awards.ts
@@ -6,34 +6,50 @@ export const sectionHeaderContent = {
 export const awardsCardContent = [
   {
     id: 1,
-    title: "School of Eminence Award",
-    img: "/images/award-image.png",
+    title: "Ivy League Status by EducationWorld India School Rankings 2025-26",
+    img: "/images/awards/ivy-league-status.jpg",
     subTitle:
-      "A celebration of our journey towards nurturing young minds with vision, values, and endless possibilities.",
+      "SAI International is among the six elite schools in India to be accorded the prestigious 'Ivy League' status, recognizing our commitment to comprehensive excellence.",
     year: "2025",
   },
   {
     id: 2,
-    title: "School of Eminence Award",
-    img: "/images/award-image.png",
+    title: "No. 1 School in India by EducationWorld India School Rankings",
+    img: "/images/awards/no-1-school-in-india-2024.jpg",
     subTitle:
-      "A celebration of our journey towards nurturing young minds with vision, values, and endless possibilities.",
-    year: "2025",
+      "Recognized as the premier institution nationwide, reflecting our dedication to outstanding academic results and holistic student development.",
+    year: "2024",
   },
   {
     id: 3,
-    title: "School of Eminence Award",
-    img: "/images/award-image.png",
+    title: "Best Education Brands in India by The Economic Times",
+    img: "/images/awards/best-education-brands-2023.jpg",
     subTitle:
-      "A celebration of our journey towards nurturing young minds with vision, values, and endless possibilities.",
-    year: "2025",
+      "Honored by The Economic Times, this award highlights our school's strong brand equity and leadership in the Indian education sector.",
+    year: "2023",
   },
   {
     id: 4,
-    title: "School of Eminence Award",
-    img: "/images/award-image.png",
+    title: "No. 1 School in India by EducationWorld India School Rankings",
+    img: "/images/awards/no-1-school-in-india-2023.jpg",
     subTitle:
-      "A celebration of our journey towards nurturing young minds with vision, values, and endless possibilities.",
-    year: "2025",
+      "A celebration of our journey towards nurturing young minds with vision, values, and endless possibilities, consistently recognized as the best.",
+    year: "2023",
+  },
+  {
+    id: 5,
+    title: "Green School Award",
+    img: "/images/awards/green-school-award.jpg",
+    subTitle:
+      "Acknowledged for our dedication to environmental stewardship, sustainability, and promoting ecological awareness within the school community.",
+    year: "2023",
+  },
+  {
+    id: 6,
+    title: "The Startup Xpress Top Innovators Award by Start Up Odisha",
+    img: "/images/awards/startup-xpress-top-innovators-award.jpg",
+    subTitle:
+      "Recognizing our initiative and support for fostering innovation, entrepreneurship, and a forward-thinking mindset among our students.",
+    year: "2023",
   },
 ];
--- src/data/home/testimonials.ts
+++ src/data/home/testimonials.ts
@@ -6,20 +6,82 @@ export const sectionHeaderContent = {
 export const testimonials = [
   {
     id: 1,
-    videoUrl: "/testimonials/mrs-haokip",
-    thumbnail: "/images/test-video.png",
-    parentName: "Mrs. Haokip",
-    designation: "Policewomen",
+    videoUrl:
+      "https://www.youtube.com/watch?v=CabF53vBb40&list=PLu5iW33-MlPMoNxx17ck6aqLd4NY9Ls-m&index=2",
+    thumbnail: "images/testimonials/mr-sarkar.jpg",
+    parentName: "Mr. Sarkar",
+    designation: "Father of Vivaan Moa",
     message:
-      "Sending my daughter to SAI was the best decision we made. The teachers are so supportive and genuinely care about each child's success. The engaging curriculum sparked a love for learning in her, and she's thriving academically and socially. We're grateful for the positive impact SAI has had on our child's life.",
+      "SAI International School is one of the best schools in the country.",
   },
   {
     id: 2,
-    videoUrl: "/testimonials/mr-john-doe",
-    thumbnail: "/images/test-video.png",
-    parentName: "Mr. John Doe",
-    designation: "Parent",
+    videoUrl:
+      "https://www.youtube.com/watch?v=-yBDeC8lRW8&list=PLu5iW33-MlPMoNxx17ck6aqLd4NY9Ls-m&index=24",
+    thumbnail: "images/testimonials/sambit-mohanty.jpg",
+    parentName: "Dr. Sambit Mohanty",
+    designation: "Director of Oncopatholgy, AMRI Hospital",
     message:
-      "SAI has transformed the way my son approaches learning. He enjoys going to school every day and talks excitedly about his classes. The supportive teachers and positive environment have made a huge difference.",
+      "Our teachers are the superstars who are making the future journalists, doctors, engineers, academicians, and professionals in various fields.",
+  },
+  {
+    id: 3,
+    videoUrl:
+      "https://www.youtube.com/watch?v=K3APuSmcKoM&list=PLu5iW33-MlPMoNxx17ck6aqLd4NY9Ls-m&index=53",
+    thumbnail: "images/testimonials/madhumita-mohapatra.jpg",
+    parentName: "Mrs. Madhumita Mohapatra",
+    designation: "Mother of Mugdha Mohapatra",
+    message:
+      "I will definitely advise as a parent: think independently, do not listen to what others are saying, listen to your heart, work for it, focus on it, and you will definitely achieve it.",
+  },
+  {
+    id: 4,
+    videoUrl:
+      "https://www.youtube.com/watch?v=NX95TP9VZzM&list=PLu5iW33-MlPMoNxx17ck6aqLd4NY9Ls-m&index=54",
+    thumbnail: "images/testimonials/ranjana-chopra.jpg",
+    parentName: "Ms. Ranjana Chopra",
+    designation: "IAS, Secretary, Department of Cooperation",
+    message:
+      "I think it is a good idea to involve children in the management of such events right from the beginning. That builds up their self-confidence and gives them a perspective on how events are managed, helping them identify what they would like to do with their lives.",
+  },
+  {
+    id: 5,
+    videoUrl:
+      "https://www.youtube.com/watch?v=oqqsGACymRQ&list=PLu5iW33-MlPMoNxx17ck6aqLd4NY9Ls-m&index=55",
+    thumbnail: "images/testimonials/rajendra-sharma.jpg",
+    parentName: "Dr. Rajendra Prasad Sharma",
+    designation: "IPS, DG Police",
+    message:
+      "The students should achieve an all-round development of their personality.",
+  },
+  {
+    id: 6,
+    videoUrl:
+      "https://www.youtube.com/watch?v=khljKv9Ec1Q&list=PLu5iW33-MlPMoNxx17ck6aqLd4NY9Ls-m&index=59",
+    thumbnail: "images/testimonials/sidhartha-kanungo.jpg",
+    parentName: "Shri Sidhartha Kanungo",
+    designation: "Father of Parthabi Kanungo",
+    message:
+      "I am extremely satisfied with the performance of my daughter and the atmosphere of the school. Youngsters should work hard and, if any doubt arises, they should ask their parents as well as teachers. They should not hide anything; that is my suggestion.",
+  },
+  {
+    id: 7,
+    videoUrl:
+      "https://www.youtube.com/watch?v=ZBIAutp0uY0&list=PLu5iW33-MlPMoNxx17ck6aqLd4NY9Ls-m&index=1",
+    thumbnail: "images/testimonials/mrs-gupta.jpg",
+    parentName: "Mrs. Gupta",
+    designation: "Mother of Aarav & Arnav Gupta ",
+    message:
+      "We can see many positive changes, like improved discipline and academics. The younger one is doing very well, and the elder one has significantly improved his focus and time management.",
+  },
+  {
+    id: 8,
+    videoUrl:
+      "https://www.youtube.com/watch?v=dVsZwBlhsfk&list=PLu5iW33-MlPMoNxx17ck6aqLd4NY9Ls-m&index=5",
+    thumbnail: "images/testimonials/mr-agrawal.jpg",
+    parentName: "Mr. Agrawal",
+    designation: "Father of Shreya Agrawal",
+    message:
+      "The benefits of staying in boarding are that children learn how to manage themselves from all points of view. I've seen a lot of development and 360-degree development in her here.",
   },
 ];
