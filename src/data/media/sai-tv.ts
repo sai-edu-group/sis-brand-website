33845b291d588106ffb077dfaed4c3c7ecb52bb0
Divya Prakash <divyaprakashdash@gmail.com>
2025-11-20 16:58:25 +0530

    updated SAITV Content

--- src/data/media/sai-tv.ts
+++ src/data/media/sai-tv.ts
@@ -9,6 +9,7 @@ export const saiTVContent = {
   content: `<p class="fade-in">SAI TV is one of the most stimulating clubs that enables students to get an exposure on the basics of film making and television production. It focuses on enriching student’s knowledge on the 4 P’s of creating television programs, namely, Planning, Production, Programming and Publicity.</p>
   <p class="fade-in">Through activities such as panel discussion, anchoring, compering, interviews, studio discussions, outdoor recordings etc., students gain a hands-on experience of script writing, scouting, audio, video & lighting, use of camera, editing, studio production, digital cinematography as well as producing and directing. Furthermore, eminent National and International personalities have been interviewed in this studio, thereby giving the students a real time feel of working in a studio.</p>
   <p class="fade-in">Through activities such as panel discussion, anchoring, compering, interviews, studio discussions, outdoor recordings etc., students gain a hands-on experience of script writing, scouting, audio, video & lighting, use of camera, editing, studio production, digital cinematography as well as producing and directing. Furthermore, eminent National and International personalities have been interviewed in this studio, thereby giving the students a real time feel of working in a studio.</p>
+  <h1>hiii</h1>
   `,
   landscapeImg: "/images/media/sai-tv/sai-tv.jpg",
 };
