650fbc149d46d2656d77f42c5221d8c4313a7422
Divya Prakash Dash <divyaprakashdash@Divyas-MacBook-Air.local>
2025-11-24 11:38:50 +0530

    Mac Testing

--- src/data/media/radio.ts
+++ src/data/media/radio.ts
@@ -6,7 +6,7 @@ export const sectionHeaderContent = {
 
 export const radioOrangeContent = {
   id: 1,
-  content: `<p class="fade-in">Radio Orange 99.9 is an in-house virtual radio station, operated by the students under faculty supervision. It is the first School radio broad casting system in the state of Odisha and is a brilliant training ground for those students who dream of pursuing a career in broad casting. It gives them a great platform to share their thoughts and views and at the same time develop their communication skills along with unleashing their creativity.
+  content: `<p class="fade-in">RadioOrange 99.9 is an in-house virtual radio station, operated by the students under faculty supervision. It is the first School radio broad casting system in the state of Odisha and is a brilliant training ground for those students who dream of pursuing a career in broad casting. It gives them a great platform to share their thoughts and views and at the same time develop their communication skills along with unleashing their creativity.
 </p>
   <p class="fade-in">Students from class VI onwards are taught how to entertain audience through the magic of voice and content with spontaneity. They get to know the various techniques of mike placement, voice modulation, diction, clarity of speech etc. They are guided on how to select a topic, prepare an efficacious script, and train the voice to make it sound interesting so as to keep the audience engrossed as well as learn how to speak impromptu.
 </p>
