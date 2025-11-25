acbbe3ea84e8491c7a79d95b93b1e4ea23fa72bb
Divya Prakash Dash <divyaprakashdash@Divyas-MacBook-Air.local>
2025-11-25 19:37:00 +0530

    SAIMUN Content update

--- src/data/about/flagship.ts
+++ src/data/about/flagship.ts
@@ -24,7 +24,17 @@ export const flagshipContent = [
       "Eastern India’s largest International MUN, accredited by UNESCO and UNIC. It promotes global awareness, diplomacy, and leadership among 600+ delegates globally.",
     img: "/images/flagship/saimun.jpg",
     landscapeImg: "/images/flagship/saimun.jpg",
-    content: `<p>Life in a hostel means a whole panorama of experiences that span across making friends for life, and gaining the confidence to face life independently. Hence, we lay tremendous emphasis on creating a warm, welcoming, invigorating and home-like atmosphere in the hostels. ‘A home away from home’ where the children develop the confidence to be independent, be it through friendships, food, belongings, rooms, joys, sorrows, challenges or other uncountable experiences of growing-up together.<br>There are separate, comfortable and fully air-conditioned hostels for boys and girls, where the students follow a routine life with discipline. Rana Pratap and Chandra Gupta Maurya hostel for boys while for girls it’s Sushila Devi and Ramadevi.</p>`,
+    content: `<p>SAI Model United Nations Conference is the annual MUN of the School. Initiated in the year 2013, SAIMUN has leveraged both in scale and grandeur over the years to become Eastern India’s largest International MUN. Every year more than 600 delegates from across the globe participate in the mega event.</p>
+    <p>SAIMUN is a 3-day conceptual replication of the United Nations (UN) International Committees where students from Class IX to XII assume the role of UN representatives and the members of the International bodies, to deliberate on challenging global issues and come up with solutions through consensus.</p>
+    <p><strong>Accreditation –</strong> It is a duly accredited registered event of UNESCO and the United Nations Information Centre for India and Bhutan (UNIC), under the aegis of the UN. The theme of the event is Vasudhaiva Kutumbakam – One World, One Family.</p>
+    <p><strong>Objective – </strong>SAIMUN is a challenging platform for the students to build leadership skills; increase global literacy and expand critical thinking to foster international co-operation in solving global problems. Students get to participate in challenging debates on historic as well as contemporary global issues to envisage innovative perspective to comprehend the world we live in.</p>
+    <p>A lot of global interactions are organized during the Conference which complements the experiences of the delegates.</p>
+    <p>Global Village where International delegates showcase their culture and tradition through performances, PPTs and accessories.</p>
+    <p>Social Connect activities, where International Delegates along with SAIoneers work together for the society.</p>
+    <p>Inclusivity, where students from a Special School are invited.</p>
+    <p>Motivational Talk by renowned Speakers to inspire the participants.</p>
+    <p><a href="https://saimun.in/" target="_blank" >Visit Site</a></p>
+    `,
   },
   {
     id: 3,
