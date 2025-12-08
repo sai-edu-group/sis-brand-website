bf13898714cdb49e3e0019db8e635ec5966f443c
Divya Prakash Dash <divyaprakashdash@Divyas-MacBook-Air.local>
2025-12-08 11:33:09 +0530

    Globalpartners Round Square content updated

--- src/data/global-connect/global-partners.ts
+++ src/data/global-connect/global-partners.ts
@@ -44,7 +44,12 @@ export const globalPartnersContent = [
     landscapeImg: "/images/global-connect/global-partners/round-square.jpg",
     description:
       "Member of a worldwide network connecting schools for global collaboration and leadership.",
-    content: `<p class="fade-in">Member of a worldwide network connecting schools for global collaboration and leadership.</p>`,
+    content: `<p class="fade-in">Round Square is an internationally diverse network of 200 like-minded schools in 50 countries on six continents that connect and collaborate to offer world-class programs and experiences. The Member Schools are characterised by a shared belief in an approach to education built around SIX IDEALS - Internationalism, Democracy, Environmentalism, Adventure, Leadership and Service.</p>
+    <p>SAI International School is honoured to receive the prestigious affiliation from Round Square, UK, the only school from Eastern India to be bestowed with the coveted membership. It brings in additional opportunities for students and teachers in learning and personal development both in and beyond the classroom. The school now becomes a part of a broader movement for positive change towards a well-rounded education that focuses beyond academic achievements.</p>
+    <p>Being a Round Square School brings opportunities for students of SAI International School to develop global competence, character and understanding, through real-life meaningful activities. These activities help in developing skills and experience that serve them both in university applications and study, and in their future careers. The Member Schools motivate students into practical experiences, addressing real-world issues and inspiring them to become agents of positive change. Our students get numerous occasions to attend conferences and events at national, regional and global levels. They also get exposure to different cultures and viewpoints through student exchanges and service projects. These exchange visits help in sharing of best practices, culture and learning beyond the boundaries and limitations.</p>
+
+
+`,
   },
   {
     id: 5,
