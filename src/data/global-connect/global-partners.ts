933f6dfa767c25fc484f77025a424eae3f11d613
Divya Prakash <divyaprakashdash@gmail.com>
2025-12-08 15:57:46 +0530

    Affiliations CBSE Content updated

--- src/data/global-connect/global-partners.ts
+++ src/data/global-connect/global-partners.ts
@@ -13,7 +13,9 @@ export const globalPartnersContent = [
     landscapeImg: "/images/global-connect/global-partners/unesco.jpg",
     description:
       "Part of the UNESCO ASPnet, promoting global citizenship, peace, and shared learning.",
-    content: `<p class="fade-in">Part of the UNESCO ASPnet, promoting global citizenship, peace, and shared learning.</p>`,
+    content: `<p class="fade-in">The UNESCO Associated Schools Network (ASPnet) links educational institutions across the world around a common goal to build the defences of peace in the minds of children and young people. The ASPnet member schools in various countries work in support of international understanding, peace, intercultural dialogue, sustainable development and quality education in practice. ASPnet operates at both international and national levels.</p>
+    <p><strong>SAI International School is privileged to become a UNESCO ASPnet School since April 2015.</strong> The School strives to associate with the best forums of the World to provide students with extended platforms of learning. The Membership is based on a firm commitment by the school to promote the ideals and values of UNESCO by reinforcing the humanistic, ethical, cultural and international dimensions of education. The Member Schools contribute towards the development of innovative educational content and pioneer new teaching methods and learning approaches to improve practices to become agents of change in their communities.</p>
+    `,
   },
   {
     id: 2,
@@ -24,7 +26,10 @@ export const globalPartnersContent = [
       "/images/global-connect/global-partners/cambridge-english.jpg",
     description:
       "Annual program offering Cambridge English assessments for language proficiency.",
-    content: `<p class="fade-in">Annual program offering Cambridge English assessments for language proficiency.</p>`,
+    content: `<p class="fade-in"><strong>Cambridge English Language Assessment (CELA) </strong> is an annual program of SAI International School since the year 2009 in which the students get an opportunity to appear for a Cambridge University examination in English meant for speakers of other languages across the world. Every year hundreds of students register for these examinations, who are thoroughly trained through 20 preparatory classes.</p>
+    <p>These tests assess all four language skills like listening, reading, writing and speaking which helps to provide a comprehensive picture of the learner’s overall level of English not only to secure a bright future but also meet the global standards. <strong>Students from Classes II to XII are eligible to take part in the assessment test </strong> and receive certificates from the University of Cambridge. The students appear for various levels like YLE (Starters, Movers and Flyers), Key for Schools (KET), Preliminary for Schools (PET), Business English Certificate (BEC) Preliminary and BEC Vantage. CELA offers students a structured way to improve their English language skills.</p>
+    
+    `,
   },
   {
     id: 3,
