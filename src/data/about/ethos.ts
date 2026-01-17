79b2a9dfcad4a66bb18a4cf005b7624cbe0cfd6e
Mohdhussain <m.sunesara@somaiya.edu>
2026-01-17 12:47:22 +0530

    fixes

--- src/data/about/ethos.ts
+++ src/data/about/ethos.ts
@@ -4,10 +4,27 @@ export const ethosDetails = {
   },
   ethosContent: {
     title: "Ethos of SAI",
-    content: `<p><strong>The ethos of SAI International School is the holistic development of the body, mind and soul of the children.</strong> To achieve the highest level of health, well-being, and purpose in life, the school follows the basic principle of focusing on creating a balance in the six main areas that make up a person’s life: Physical, Social, Emotional, Spiritual, Intellectual, and Occupational (Purposeful).</p><br>
-<p><strong>SAI International School</strong>, applies this understanding and knowledge of the various dimensions of wellness, to address the changing needs of the children through meaningful, focused activities that engage them and offer support in these different areas to nurture the body, mind and soul. Whole-person wellness strives to help each child achieve and maximize their own individual potential as well as reach greater heights, with a new perspective towards life.</p><br>
+    content: `<p><strong>The ethos of SAI International School is the holistic development of the body, mind and soul of the children.</strong> To achieve the highest level of health, well-being, and purpose in life, the school follows the basic principle of focusing on creating a balance in the six main areas that make up a person’s life: Physical, Social, Emotional, Spiritual, Intellectual, and Occupational (Purposeful).</p>
+<p><strong>SAI International School</strong>, applies this understanding and knowledge of the various dimensions of wellness, to address the changing needs of the children through meaningful, focused activities that engage them and offer support in these different areas to nurture the body, mind and soul. Whole-person wellness strives to help each child achieve and maximize their own individual potential as well as reach greater heights, with a new perspective towards life.</p>
 <p>The School enables holistic growth and development through a progressive curriculum, innovative instructional practices and character building processes, following multiple learning styles. Encouraging and endowing each child to initiate a lifelong pursuit of learning, discovery, innovation and expression in the truest forms, is the belief on which our processes are based.</p>
-<br><p>In strive for excellence; SAI International School has introduced <strong>SAI Learning Foundation</strong> <strong>and SAI Culture and Value Building</strong>.</p> <br>
+<p>In strive for excellence; SAI International School has introduced <strong>SAI Learning Foundation</strong> <strong>and SAI Culture and Value Building</strong>.</p> 
+<p>In developing these concepts, we transform the inner virtues in children, enabling and empowering them to project their potential for greater insight and inspiration.</p>
+
+
+<p><strong>Guru Shishya Parampara for Harmonising Traditional Human Values</strong></p>
+
+<p>The school practices the Guru Shishya Parampara, harmonizing traditional human values of integrity and humility, with the best global practices, thus enhancing the capacity to <strong>lead India better through education</strong>. Our students and faculty truly depict unity in diversity. We have students from across the nation and the best of teachers too, from various parts of India to impart quality education.</p>
+
+<p>The school emphasizes that education should be in accordance with the needs of the 21st century, helping create dynamic citizens with the ability to lead the country forward. The school lays strong emphasis on providing a free environment for the child to develop all the latent faculties to the maximum, and the curriculum possesses elements of creativity and educational expression, which breathe new life into the subjects, hence making learning a joyful experience. The learning imbibed in this manner, ensures the retention of concepts for a lifetime. <strong>SAI International believes that education is the manifestation of the perfection already present in any child, and the school is just an enabler to bring out and hone the talent.</strong></p>
+
+<p>SAI International creates a lively atmosphere for the development and use of intellectual curiosity, imagination and technical skill, thus motivating the learners to accept goals that stretch their talents and energies to greater heights thus creating thought leaders for the future.</p>
+
+<p><strong>SAIoneers Educating Volunteerism with Affirmation (SEVA)</strong></p>
+
+
+<p>Our edification encourages fearlessness of mind, strengthening of conscience and integrity of purpose.<strong>S</strong>AIoneers <strong>E</strong>ducating <strong>V</strong>olunteerism with <strong>A</strong>ffirmation leads the SAIoneers to educate the world to volunteer for service to mankind with affirmation.</p>
+
+<p>We believe that education is the collective embodiment of humankind, values and learning, together intertwined, to attain the desired goal of leading India through education and shaping responsible citizens of tomorrow, well equipped with the much needed 21st century skills.</p>
       `,
   },
 };
