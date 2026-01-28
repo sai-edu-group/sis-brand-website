789f6b4b5698f9efc6ff99cc44f7c527f4040810
omisan01 <m.sunesara@somaiya.edu>
2026-01-28 13:06:49 +0530

    lighthouse

--- src/data/home/stats.ts
+++ src/data/home/stats.ts
@@ -8,26 +8,26 @@ export const statsCard = [
     id: 1,
     title: "of students scored above 90%",
     statsCount: "44.6%",
-    img: "/images/stats-image-1.png",
+    img: "/images/stats-image-1.jpg",
   },
   {
     id: 2,
     title: "National and International competitive examinations",
     statsCount: "335+",
     subTitle: "(JEE/NEET/CLAT/SAT)",
-    img: "/images/stats-image-2.png",
+    img: "/images/stats-image-2.jpg",
   },
   {
     id: 3,
     title: "Rank No.1 School for 5 Consecutive Years",
     statsCount: "#1",
-    img: "/images/stats-image-3.png",
+    img: "/images/stats-image-3.jpg",
   },
   {
     id: 4,
     title: "sq.ft. of Square Feet of campus with world-class Sports Facilities",
     statsCount: "335K+",
-    img: "/images/sports-facilities.jpg",
+    img: "/images/sports-facilities.png",
   },
   {
     id: 5,
@@ -39,6 +39,6 @@ export const statsCard = [
     id: 6,
     title: "Ivy League and top Global Universities.",
     statsCount: "20+",
-    img: "/images/stats-image-4.png",
+    img: "/images/stats-image-4.jpg",
   },
 ];
