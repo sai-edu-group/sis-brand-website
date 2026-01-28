cacf43abe835e96a9046d0e115259eea772a2ef6
omisan01 <m.sunesara@somaiya.edu>
2026-01-28 14:55:31 +0530

    lighthouse

--- src/data/home/stats.ts
+++ src/data/home/stats.ts
@@ -27,7 +27,7 @@ export const statsCard = [
     id: 4,
     title: "sq.ft. of Square Feet of campus with world-class Sports Facilities",
     statsCount: "335K+",
-    img: "/images/sports-facilities.jpg",
+    img: "/images/sports-facilities (1).jpg",
   },
   {
     id: 5,
