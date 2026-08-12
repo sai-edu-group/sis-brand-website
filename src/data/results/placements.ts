46bbc5e65061ecceb1817cc1748f149f14036a41
divyaprakashdash <divyaprakashdash@gmail.com>
2026-08-12 17:39:08 +0530

    Placement details updated

--- src/data/results/placements.ts
+++ src/data/results/placements.ts
@@ -8,34 +8,30 @@ import type { PlacementData } from "@/types/results/results";
 export const placements: PlacementData[] = [
   {
     id: 1,
-    name: "2025 Batch",
-    image: "/images/results/placements/2025-batch.jpg",
+    name: "2026 Batch",
+    image: "/images/results/placements/2026.jpg",
   },
   {
     id: 2,
-    name: "2024 Batch",
-    image: "/images/results/placements/2024-batch.jpg",
+    name: "2025 Batch",
+    image: "/images/results/placements/2025.jpg",
   },
   {
     id: 3,
-    name: "2023 Batch",
-    image: "/images/results/placements/2023-batch.jpg",
+    name: "2024 Batch",
+    image: "/images/results/placements/2024.jpg",
   },
   {
     id: 4,
-    name: "2022 Batch",
-    image: "/images/results/placements/2022-batch.jpg",
+    name: "2023 Batch",
+    image: "/images/results/placements/2023.jpg",
   },
   {
     id: 5,
-    name: "2021 Batch",
-    image: "/images/results/placements/2021-batch.jpg",
+    name: "2022 Batch",
+    image: "/images/results/placements/2022.jpg",
   },
-  {
-    id: 6,
-    name: "2020 Batch",
-    image: "/images/results/placements/2020-batch.jpg",
-  },
+
 ];
 
 export const placementsSectionHeader: SectionHeaderMainData = {
